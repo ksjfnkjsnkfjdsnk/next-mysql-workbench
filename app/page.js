84f52699334830842739ea1640f92d6766f0144c
ÂLucas Cabral <lucassimple1995@gmail.com>
2024-11-29 19:45:52 -0300

    Version 1.0.0

--- app/page.js
+++ app/page.js
@@ -1,34 +1,44 @@
-import axios from "axios";
+// import axios from "axios";
 
-const getData = async () => {
-try {
-  const results = await axios.get(`http://localhost:3000/api/neon`);
+// const getData = async () => {
+// try {
+//   const results = await axios.get(`http://localhost:3000/api/neon`);
 
-  return results.data.result
-} catch (error) {
-  console.log(error.message)
-}
-}
+//   return results.data.result
+// } catch (error) {
+//   console.log(error.message)
+// }
+// }
 
-const Home = async () => {
+// const Home = async () => {
 
-  const users = await getData();
-  console.log(users)
+//   const users = await getData();
+//   console.log(users)
 
+//   return (
+//     <div>
+
+//       <h1> Lista de usuarios leida de Neon </h1>
+
+//       {users.map((item, indx) => (
+//         <div key={indx}>
+//           <p> Nombre: {item.nombre} </p>
+//           <p> Email: {item.email} </p>
+//         </div>
+//       ))}
+
+//     </div>
+//   )
+// };
+
+// export default Home
+
+const Home = () => {
   return (
     <div>
-
-      <h1> Lista de usuarios leida de Neon </h1>
-
-      {users.map((item, indx) => (
-        <div key={indx}>
-          <p> Nombre: {item.nombre} </p>
-          <p> Email: {item.email} </p>
-        </div>
-      ))}
-
+      <h1>Home</h1>
     </div>
-  )
+  );
 };
 
-export default Home
+export default Home;
